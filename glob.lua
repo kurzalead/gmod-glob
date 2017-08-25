3a2c2f5461bb94c39c7a773cf0b5e0b9f3134df9
Kevin <1412696+kurzyx@users.noreply.github.com>
2017-08-25 17:32:11 +0200

    Update glob.lua

--- glob.lua
+++ glob.lua
@@ -1,6 +1,6 @@
 --[[
 
-Copyright 2017 kurzyx
+Copyright 2017 kurzyx https://github.com/kurzyx
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
